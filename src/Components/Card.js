0f68099da65a6c7d877689fd8b93bb2745d8b01f
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-06-13 13:26:34 +0300

    CSS Solution Device Friendly

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,6 +2,7 @@ import Description from './Description.js';
 import Submit from './Submit.js';
 
 import BackgroundDesktop from '../../src/bg-intro-desktop.png';
+import BackgroundMobile from '../../src/bg-intro-mobile.png';
 
 import classes from './Card.module.css';
 
@@ -9,6 +10,7 @@ const Card = () => {
   return (
     <div className={classes.body}>
       <img src={BackgroundDesktop} className={classes.desktop} />
+      <img src={BackgroundMobile} className={classes.mobile} />
       <Description />
       <Submit />
     </div>
--- src/Components/Submit.js
+++ src/Components/Submit.js
@@ -5,8 +5,9 @@ const Submit = () => {
     <div className={classes.submitCard}>
       <div className={classes.movingCard}>
         <div className={classes.trial}>
-          <h3>Try it free 7 days </h3>
-          <h4>then $20/mo. thereafter</h4>
+          <h3>
+            Try it free 7 days <h4>then $20/mo. thereafter</h4>
+          </h3>
         </div>
         <form className={classes.form}>
           <input type="text" placeholder="First Name" className={classes.firstName}></input>
@@ -15,8 +16,9 @@ const Submit = () => {
           <input type="password" placeholder="Password" className={classes.password}></input>
           <h2 className={classes.claim}>CLAIM YOUR FREE TRIAL</h2>
           <div className={classes.terms}>
-            <p className={classes.termsGrey}>By clicking the button, you are agreeing to our </p>
-            <p className={classes.termsGreen}> Terms and Services</p>
+            <p className={classes.termsGrey}>
+              By clicking the button, you are agreeing to our <p className={classes.termsGreen}> Terms and Services</p>{' '}
+            </p>
           </div>
         </form>
       </div>
